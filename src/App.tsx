a3e5e146c75dfb933ddc6c54a3ccde566f69a5d1
Benjamin Haines <bhaines@yext.com>
2024-08-08 12:56:50 -0400

    controlled filters example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
 import { SaasProvider } from "@saas-ui/react";
-import { theme } from "@saas-ui-pro/react";
+import { theme, FiltersProvider, Filter } from "@saas-ui-pro/react";
+import { Spacer } from "@chakra-ui/react";
+import {
+  Page,
+  PageHeader,
+  Toolbar,
+  FiltersAddButton,
+  ActiveFiltersList,
+} from "@saas-ui-pro/react";
 import "./App.css";
+
+function ListPage() {
+  const filters = [
+    {
+      id: "type",
+      label: "Contact is lead",
+      activeLabel: "Contact",
+      value: "lead",
+    },
+    {
+      id: "type",
+      label: "Contact is customer",
+      activeLabel: "Contact",
+      value: "customer",
+    },
+  ];
+
+  const activeFilters: Filter[] = [
+    { id: "type", operator: "is", value: "lead" },
+    { id: "type", operator: "isNot", value: "customer" },
+  ];
+
+  return (
+    <FiltersProvider
+      filters={filters}
+      onChange={console.log}
+      activeFilters={activeFilters}
+    >
+      <FiltersAddButton />
+      <ActiveFiltersList />
+    </FiltersProvider>
+  );
+}
 
 function App() {
   return (
     <SaasProvider theme={theme}>
-      <div>hi</div>
+      <ListPage />
     </SaasProvider>
   );
 }
