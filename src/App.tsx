d9004b374afa32c4f48f29fedf321f0bc6d35fb0
Benjamin Haines <bhaines@yext.com>
2024-12-30 11:31:40 -0500

    modal + filters z-index

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,65 @@
-import { SaasProvider } from "@saas-ui/react";
-import { theme } from "@saas-ui-pro/react";
+import { useMemo } from "react";
+import {
+  useDisclosure,
+  Modal,
+  ModalHeader,
+  ModalContent,
+  ModalOverlay,
+  ModalBody,
+  ModalCloseButton,
+  ModalFooter,
+  Button,
+} from "@chakra-ui/react";
+import { FiltersProvider, FiltersAddButton, ActiveFiltersList, FilterType } from "@saas-ui-pro/react";
 import "./App.css";
 
 function App() {
+  const { onClose, onOpen, isOpen } = useDisclosure();
+  const filters = useMemo(
+    () => [
+      {
+        id: 'status',
+        label: 'Status',
+        type: 'enum' as FilterType,
+        items: [
+          {
+            id: 'new',
+            label: 'New',
+          },
+          {
+            id: 'active',
+            label: 'Active',
+          },
+        ],
+      },
+    ],
+    []
+  )
+
   return (
-    <SaasProvider theme={theme}>
-      <div>hi</div>
-    </SaasProvider>
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FiltersProvider filters={filters}>
+              <FiltersAddButton listProps={{zIndex: "popover"}} />
+                <ActiveFiltersList />
+            </FiltersProvider>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
 
